d082c32a5d0da7229e194d651f85185709a9566b
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-29 14:17:29 -0600

    se añadio la funcionalidad de editar las citas o pacientes

--- src/components/PatientForm.tsx
+++ src/components/PatientForm.tsx
@@ -2,21 +2,45 @@ import { useForm} from "react-hook-form"
 import Error from "./Error"
 import { DraftPatient } from "../types"
 import { usePatienStore } from "../store"
+import { useEffect } from "react"
 export default function PatientForm() {
   
-    const {addPatient} = usePatienStore()
-    const { register, handleSubmit, formState: {errors}, reset } = useForm<DraftPatient>()
+    const addPatient = usePatienStore(state => state.addPatient)
+    const activeId = usePatienStore(state => state.activeId)
+    const patients = usePatienStore(state => state.patients)
+    const updatePatient = usePatienStore(state => state.updatePatient)
+
+    const { register, handleSubmit, setValue, formState: {errors}, reset } = useForm<DraftPatient>()
+
+    useEffect(() =>{
+        if(activeId){
+            const activePatient = patients.filter(patient => patient.id === activeId)[0]
+            setValue('name', activePatient.name)
+            setValue('caretaker', activePatient.caretaker)
+            setValue('email', activePatient.email)
+            setValue('date', activePatient.date)
+            setValue('symptoms', activePatient.symptoms)
+
+        }
+    }, [activeId])
     
     const registerPatient = (data: DraftPatient) => {
-        addPatient(data)
+        if(activeId){
+            updatePatient(data)
+        }else{
+            addPatient(data)
+        }
+        
         reset()
     }
+
+
 
     return (
       <div className="md:w-1/2 lg:w-2/5 mx-5">
           <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
   
-          <p className="text-lg mt-5 text-center mb-10">
+          <p className="text-xl mt-5 mb-10 text-center">
               Añade Pacientes y {''}
               <span className="text-indigo-600 font-bold">Administralos</span>
           </p>
--- src/store.ts
+++ src/store.ts
@@ -1,20 +1,25 @@
 import {create} from 'zustand'
+import { devtools } from 'zustand/middleware'
 import {v4 as uuidv4} from 'uuid'
 import { DraftPatient, Patient } from './types'
 
 
 type PatientState = {
     patients: Patient[]
+    activeId: Patient['id']
     addPatient: (data: DraftPatient) => void
     deletePatient: (id: Patient['id']) => void
+    getPatientById: (id: Patient['id']) => void
+    updatePatient: (id: DraftPatient) => void 
 }
 
 const createPatient = (patient: DraftPatient) : Patient => {
     return {...patient, id: uuidv4() }
 }
 
-export const usePatienStore = create<PatientState> ((set) => ({
+export const usePatienStore = create<PatientState>()(devtools ((set) => ({
     patients: [],
+    activeId: '',
     addPatient: (data) => {
        const newPatient = createPatient(data) 
        set((state) => ({
@@ -25,5 +30,17 @@ export const usePatienStore = create<PatientState> ((set) => ({
         set((state) => ({
             patients: [...state.patients.filter(patient => patient.id !== id)]
         }))
+    },
+    getPatientById: (id) => {
+        set(() => ({
+            activeId: id
+        }))
+    },
+    updatePatient: (data) => {
+        set((state) => ({
+            patients: state.patients.map((patient) => patient.id === state.activeId ? {id: state.activeId, ...data } :patient),
+            activeId: '',
+        }))
     }
-})) 
+
+})))
