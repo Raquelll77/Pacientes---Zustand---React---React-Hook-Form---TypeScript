330d216d720632ed038ce769912e667f8bf2116f
Raquel Alvarado <zilpa.alvarado@unah.hn>
2025-01-29 08:38:41 -0600

    añadiendo id y agregando la informacion en el hook

--- src/components/PatienList.tsx
+++ src/components/PatienList.tsx
@@ -1,5 +1,8 @@
-
+import { usePatienStore } from "../store"
 export default function PatienList() {
+
+  const patients = usePatienStore(state => state.patients)  
+  console.log(patients)
   return (
     <div>PatienList</div>
   )
